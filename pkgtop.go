8fa1d2631a81c3ffbf2f78324fd00456ca0c91cf
KeyLo99 <keylo99official@gmail.com>
2019-07-20 14:49:49 +0300

    Add & initialize termui library

--- pkgtop.go
+++ pkgtop.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	ui "github.com/gizak/termui/v3"
 )
 
 func main() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
 	fmt.Println("pkgtop")
 }
