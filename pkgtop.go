995eab38f0a1de5263d4df51154bd1ec9d8b3125
KeyLo99 <keylo99official@gmail.com>
2019-07-20 22:48:13 +0300

    Remove fmt library from main file

--- pkgtop.go
+++ pkgtop.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	ui "github.com/gizak/termui/v3"
 )
 
 func main() {
-	fmt.Println("pkgtop")
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
