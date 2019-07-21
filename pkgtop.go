b140c972d81853875e1c76be27e4443c36b99644
KeyLo99 <keylo99official@gmail.com>
2019-07-22 02:58:42 +0300

    Create the function getDfText for refactor & optimization

--- pkgtop.go
+++ pkgtop.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gizak/termui/v3/widgets"
 )
 
-var diskUsageText string
+var diskUsage map[string]int
 
 func btoi(b bool) int {
 	if b {
@@ -29,13 +29,24 @@ func maxValMap(m map[string]int) string {
     return key
 }
 
+func getDiskUsage(diskUsage map[string]int, width int) string {
+	var diskUsageText string
+	for k, v := range diskUsage {
+		diskUsageText += fmt.Sprintf(" %s%s[%s %s%d%%] \n", k, 
+			strings.Repeat(" ", len(maxValMap(diskUsage)) + 5 -len(k)), 
+			strings.Repeat("|", ((width/3)*v)/100), 
+			strings.Repeat(" ", (width/3)-((width/3)*v)/100 + btoi(v < 10)), v)
+	}
+	return diskUsageText
+}
+
 func main() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 
-	diskUsage:= map[string]int{
+	diskUsage = map[string]int{
 		"dev": 0,
 		"run": 1,
 		"/dev/sda1": 75,
@@ -43,15 +54,9 @@ func main() {
 	}
 	termWidth, termHeight := ui.TerminalDimensions()
 
-	for k, v := range diskUsage {
-		diskUsageText += fmt.Sprintf(" %s%s[%s %s%d%%] \n", k, 
-			strings.Repeat(" ", len(maxValMap(diskUsage)) + 5 -len(k)), 
-			strings.Repeat("|", ((termWidth/3)*v)/100), 
-			strings.Repeat(" ", (termWidth/3)-((termWidth/3)*v)/100 + btoi(v < 10)), v)
-	}
 
 	dfText := widgets.NewParagraph()
-	dfText.Text = diskUsageText
+	dfText.Text = getDiskUsage(diskUsage, termWidth)
 	//dfText.Border = false
 
 	pkgText := widgets.NewParagraph()
@@ -77,14 +82,7 @@ func main() {
 			case "<Resize>":
 				payload := e.Payload.(ui.Resize)
 				termGrid.SetRect(0, 0, payload.Width, payload.Height)
-				diskUsageText = ""
-				for k, v := range diskUsage {
-					diskUsageText += fmt.Sprintf(" %s%s[%s %s%d%%] \n", k, 
-						strings.Repeat(" ", len(maxValMap(diskUsage)) + 5 -len(k)), 
-						strings.Repeat("|", ((payload.Width/3)*v)/100), 
-						strings.Repeat(" ", (payload.Width/3)-((payload.Width/3)*v)/100 + btoi(v < 10)), v)
-				}
-				dfText.Text = diskUsageText
+				dfText.Text = getDiskUsage(diskUsage, payload.Width)
 				ui.Clear()
 				ui.Render(termGrid)
 			}
