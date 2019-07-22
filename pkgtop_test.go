b524fe6d0cbb8917e86e1d569c992ad28820cf4f
KeyLo99 <keylo99official@gmail.com>
2019-07-22 04:23:15 +0300

    Add test function for btoi

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -1,1 +1,11 @@
 package main
+import (
+	"testing"
+)
+
+func TestBtoi(t *testing.T){
+	i := 1
+	if btoi(i == 1) != 1 ||  btoi(i != 1) != 0 {
+		t.Errorf("Unable to convert boolean to integer value")
+	}
+}
