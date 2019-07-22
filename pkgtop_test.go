534421030ec99ca9c015c19b6bd7030612a585ec
KeyLo99 <keylo99official@gmail.com>
2019-07-22 14:27:50 +0300

    Create test function of maxValMap

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -1,11 +1,25 @@
 package main
 import (
 	"testing"
+	"math/rand"
 )
 
-func TestBtoi(t *testing.T){
+func TestBtoi(t *testing.T) {
 	i := 1
 	if btoi(i == 1) != 1 ||  btoi(i != 1) != 0 {
 		t.Errorf("Unable to convert boolean to integer value")
+	}
+}
+
+func TestMaxValMap(t *testing.T) {
+	m := map[string]int{
+		"1": rand.Intn(99),
+		"2": 100,
+		"3": rand.Intn(99),
+		"4": rand.Intn(99),
+	}
+	val := maxValMap(m)
+	if val != "2" {
+		t.Errorf("Expected '2', got '%s'", val)
 	}
 }
