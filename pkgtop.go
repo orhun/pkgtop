024429aa58bddfdd54c18af5e07418588d71615e
KeyLo99 <keylo99official@gmail.com>
2019-07-22 20:06:37 +0300

    Revert back to default UI widgets

--- pkgtop.go
+++ pkgtop.go
@@ -1,65 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"math"
-	"strings"
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 )
-
-var diskUsage map[string]int
-var pkgList *widgets.List
-
-// Convert boolean value to integer.
-func btoi(b bool) int {
-	if b {
-		return 1
-	}
-	return 0
-}
-
-// Find maximum key-value pair in map.
-func maxValMap(m map[string]int) string {
-	var max int = 0
-	var key string = ""
-	for k, v := range m {
-        if max < v {
-			max = v
-			key = k
-        }
-    }
-    return key
-}
-
-// Find the maximum item size in a string array.
-func maxArrItemSize(s []string) int {
-	var arr []int
-	var max int = 0
-	for _, p := range s {
-		arr = append(arr, len(p))
-	}
-	for _, l := range arr {
-        if max < l {
-			max = l
-        }
-    }
-	return max
-}
-
-
-func getDfText(diskUsage map[string]int, width int) string {
-	var diskUsageText string
-	width = int(float64(width)/2.5)
-	for k, v := range diskUsage {
-		diskUsageText += fmt.Sprintf(" %s%s[%s %s%d%%] \n", k, 
-			strings.Repeat(" ", len(maxValMap(diskUsage)) + 1 - len(k)), 
-			strings.Repeat("|", (width*v)/100), 
-			strings.Repeat(" ", width-(width*v)/100 + btoi(v < 10)), v)
-	}
-	return diskUsageText
-}
 
 func main() {
 	if err := ui.Init(); err != nil {
@@ -67,22 +12,12 @@ func main() {
 	}
 	defer ui.Close()
 
-	diskUsage = map[string]int{
+	diskUsage := map[string]int{
 		"dev": 0,
 		"run": 1,
 		"/dev/sda1": 75,
 		"tmpfs": 4,
 	}
-	termWidth, termHeight := ui.TerminalDimensions()
-
-
-	dfText := widgets.NewParagraph()
-	dfText.Text = getDfText(diskUsage, termWidth)
-	//dfText.Border = false
-
-	pkgText := widgets.NewParagraph()
-	pkgText.Text = "~"
-	//pkgText.Border = false
 
 	pkgs := []string{
 		"apache~2.4.39-1~6.25MiB~'Fri 11 Jan 2019 03:34:39'",
@@ -97,33 +32,52 @@ func main() {
 		"docker~1:18.09.6-1~170.98MiB~'Fri 11 Jan 2019 03:34:39'",
 	}
 
-	pd := (termWidth - maxArrItemSize(pkgs))/(len(strings.Split(pkgs[0], "~"))-1)
-	for i, p := range pkgs {
-		pkg := strings.Split(p, "~")
-		pkgs[i] = ""
-		for ip, prop := range pkg {
-			pkgs[i] += prop
-			if ip != len(pkg) - 1 {
-				pkgs[i] += strings.Repeat(" ", pd - len(prop) + 1)
-			}
-		}
-	}
+	_ = diskUsage
+	_ = pkgs
 
-	pkgList = widgets.NewList()
-	pkgList.Rows = pkgs
+	dfGrid := ui.NewGrid()
+
+	g0 := widgets.NewGauge()
+	g0.Title = "Slim Gauge"
+	g0.SetRect(20, 20, 30, 30)
+	g0.Percent = 75
+	g0.BarColor = ui.ColorRed
+	g0.BorderStyle.Fg = ui.ColorWhite
+	g0.TitleStyle.Fg = ui.ColorCyan
+
 	
+	dfGrid.Set(
+		ui.NewRow(0.25,
+			ui.NewCol(1.0, g0),
+		),
+		ui.NewRow(0.25,
+			ui.NewCol(1.0, g0),
+		),
+		ui.NewRow(0.25,
+			ui.NewCol(1.0, g0),
+		),
+		ui.NewRow(0.25,
+			ui.NewCol(1.0, g0),
+		),
+	)
+
+	pkgText := widgets.NewParagraph()
+	pkgText.Text = "~"
+	//pkgText.Border = false
+
 	termGrid := ui.NewGrid()
+	termWidth, termHeight := ui.TerminalDimensions()
 	termGrid.SetRect(0, 0, termWidth, termHeight)
 	termGrid.Set(
-		ui.NewRow(0.25,
-			ui.NewCol(0.5, dfText),
-			ui.NewCol(0.5, pkgText),
+		ui.NewRow(1.0/8,
+			ui.NewCol(1.0/2, pkgText),
+			ui.NewCol(1.0/2, pkgText),
 		),
-		ui.NewRow(0.65,
-			ui.NewCol(1.0, pkgList),
+		ui.NewRow(1.0/1.5,
+			ui.NewCol(1.0/1, pkgText),
 		),
-		ui.NewRow(0.10,
-			ui.NewCol(1.0, pkgText),
+		ui.NewRow(1.0/5,
+			ui.NewCol(1.0/1, pkgText),
 		),
 	)
 	ui.Render(termGrid)
@@ -136,37 +90,7 @@ func main() {
 				return
 			case "<Resize>":
 				payload := e.Payload.(ui.Resize)
-				termGrid.SetRect(0, 0, payload.Width, payload.Height)
-				dfText.Text = getDfText(diskUsage, payload.Width)
-
-				pkgs := []string{
-					"apache~2.4.39-1~6.25MiB~'Fri 11 Jan 2019 03:34:39'",
-					"autoconf~2.69-5~2.06MiB~'Fri 11 Jan 2019 03:34:39'",
-					"automake~1.16.1-1~1598.00KiB~'Fri 11 Jan 2019 03:34:39'",
-					"bind-tools~9.14.2-1~5.85MiB~'Fri 11 Jan 2019 03:34:39'",
-					"bison~3.3.2-1~2013.00KiB~'Fri 11 Jan 2019 03:34:39'",
-					"brook~20190401-1~13.98MiB~'Fri 11 Jan 2019 03:34:39'",
-					"chafa~1.0.1-1~327.00KiB~'Fri 11 Jan 2019 03:34:39'",
-					"cmatrix~2.0-1~95.00KiB~'Fri 11 Jan 2019 03:34:39'",
-					"compton~6.2-2~306.00KiB~'Fri 11 Jan 2019 03:34:39'",
-					"docker~1:18.09.6-1~170.98MiB~'Fri 11 Jan 2019 03:34:39'",
-				}
-			
-				pd := (payload.Width - maxArrItemSize(pkgs))/(len(strings.Split(pkgs[0], "~"))-1)
-				for i, p := range pkgs {
-					pkg := strings.Split(p, "~")
-					pkgs[i] = ""
-					for ip, prop := range pkg {
-						pkgs[i] += prop
-						if ip != len(pkg) - 1 {
-							pkgs[i] += strings.Repeat(" ", int(math.Abs(float64(pd - len(prop) + 1))))
-						}
-					}
-				}
-
-		
-				pkgList.Rows = pkgs
-				
+				termGrid.SetRect(0, 0, payload.Width, payload.Height)		
 				ui.Clear()
 				ui.Render(termGrid)
 			}
--- pkgtop_test.go
+++ pkgtop_test.go
@@ -1,25 +1,4 @@
 package main
 import (
 	"testing"
-	"math/rand"
 )
-
-func TestBtoi(t *testing.T) {
-	i := 1
-	if btoi(i == 1) != 1 ||  btoi(i != 1) != 0 {
-		t.Errorf("Unable to convert boolean to integer value")
-	}
-}
-
-func TestMaxValMap(t *testing.T) {
-	m := map[string]int{
-		"1": rand.Intn(99),
-		"2": 100,
-		"3": rand.Intn(99),
-		"4": rand.Intn(99),
-	}
-	val := maxValMap(m)
-	if val != "2" {
-		t.Errorf("Expected '2', got '%s'", val)
-	}
-}
