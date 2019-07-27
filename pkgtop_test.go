f1f21cd40c3acd073a5295bb0276a5e3865d488e
KeyLo99 <keylo99official@gmail.com>
2019-07-27 18:33:03 +0300

    Refactor the variable names in test functions

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -3,14 +3,14 @@ import (
 	"testing"
 )
 
-func TestGetDfEntries(t *testing.T){
-	dfTest := []string {
+func TestGetDfEntries(t *testing.T) {
+	df := []string {
 		"x~14",
 		"y~40",
 		"z~25",
 	}
-	if getDfEntries(dfTest) == nil || 
-		len(getDfEntries(dfTest)) == 0 {
+	if getDfEntries(df) == nil || 
+		len(getDfEntries(df)) == 0 {
 		t.Error("Error occurred while parsing the values")
 	}
 }
