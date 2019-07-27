149d65813331abe6bd7ac1e7b5ae400324c224a6
KeyLo99 <keylo99official@gmail.com>
2019-07-27 18:41:13 +0300

    Add test function for getPkgListEntries

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -11,6 +11,21 @@ func TestGetDfEntries(t *testing.T) {
 	}
 	if getDfEntries(df) == nil || 
 		len(getDfEntries(df)) == 0 {
-		t.Error("Error occurred while parsing the values")
+		t.Error("Error occurred while parsing the df values")
+	}
+}
+
+func TestGetPkgListEntries(t *testing.T) {
+	pkgs := []string {
+		"val0~10~x~test1",
+		"val1~20~y~test2",
+	}
+	titles := []string{"1", "2",}
+	lists, entries := getPkgListEntries(pkgs, titles)
+	if(len(lists) != len(titles) || 
+		len(entries) != len(titles)) {
+		t.Errorf("Error occurred while parsing the pkg values")
+		t.Errorf("Expected length %d, got %d-%d", 
+			len(titles), len(lists), len(entries))
 	}
 }
