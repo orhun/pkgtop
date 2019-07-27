ae1d8863c98c70f04b3d245ff60f00cf425bcb9f
KeyLo99 <keylo99official@gmail.com>
2019-07-27 17:05:54 +0300

    Add test function for getDfEntries

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -2,3 +2,14 @@ package main
 import (
 	"testing"
 )
+
+func TestGetDfEntries(t *testing.T){
+	dfTest := []string {
+		"x~14",
+		"y~40",
+		"z~25",
+	}
+	if getDfEntries(dfTest) == nil || len(getDfEntries(dfTest)) == 0 {
+		t.Error("Error occurred while parsing the values")
+	}
+}
