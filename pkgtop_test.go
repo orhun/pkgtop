e3c52904d9724ecf13deed6514759726500b986a
KeyLo99 <keylo99official@gmail.com>
2019-07-29 01:59:17 +0300

    Update the test function of getDfEntries

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -4,14 +4,22 @@ import (
 )
 
 func TestGetDfEntries(t *testing.T) {
-	df := []string {
-		"x~14",
-		"y~40",
-		"z~25",
+	dfOutput := []string{
+		"dev dev 0 1.9G 0% /dev",
+		"run run 996K 1.9G 1% /run",
+		"/dev/sda1 /dev/sda1 51G 18G 75% /",
 	}
-	if getDfEntries(df) == nil || 
-		len(getDfEntries(df)) == 0 {
-		t.Error("Error occurred while parsing the df values")
+	dfIndex := 0
+	dfCount := 3
+	gauges, dfEntries := getDfEntries(
+		dfOutput, 
+		dfIndex, 
+		dfCount)
+	if dfEntries == nil || len(dfEntries) != dfCount || 
+		len(gauges) != dfCount {
+		t.Errorf("Error occurred while parsing the 'df' values. " + 
+			"Expected length %d, got %d-%d", 
+			dfCount, len(gauges), len(dfEntries))
 	}
 }
 
@@ -24,7 +32,7 @@ func TestGetPkgListEntries(t *testing.T) {
 	lists, entries := getPkgListEntries(pkgs, titles)
 	if(len(lists) != len(titles) || 
 		len(entries) != len(titles)) {
-		t.Errorf("Error occurred while parsing the pkg values")
+		t.Errorf("Error occurred while parsing the 'pkg' values")
 		t.Errorf("Expected length %d, got %d-%d", 
 			len(titles), len(lists), len(entries))
 	}
