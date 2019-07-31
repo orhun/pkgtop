d0e480a4f23609d261551bdbf9c865ceff147ccf
KeyLo99 <keylo99official@gmail.com>
2019-08-01 01:41:29 +0300

    Update the test slice for the package parsing

--- pkgtop.go
+++ pkgtop.go
@@ -159,14 +159,6 @@ func initUi() int {
 	pkgs := []string {
 		"apache~2.4.39-1~6.25MiB~'Fri 11 Jan 2019 03:34:39'",
 		"autoconf~2.69-5~2.06MiB~'Fri 11 Jan 2019 03:34:39'",
-		"automake~1.16.1-1~1598.00KiB~'Fri 11 Jan 2019 03:34:39'",
-		"bind-tools~9.14.2-1~5.85MiB~'Fri 11 Jan 2019 03:34:39'",
-		"bison~3.3.2-1~2013.00KiB~'Fri 11 Jan 2019 03:34:39'",
-		"brook~1-1~13.98MiB~'Fri 11 Jan 2019 03:34:39'",
-		"chafa~1.0.1-1~327.00KiB~'Fri 11 Jan 2019 03:34:39'",
-		"cmatrix~2.0-1~95.00KiB~'Fri 11 Jan 2019 03:34:39'",
-		"compton~6.2-2~306.00KiB~'Fri 11 Jan 2019 03:34:39'",
-		"docker~1-1~170.98MiB~'Fri 11 Jan 2019 03:34:39'",
 	}
 	titles := []string{"1", "2", "3", "4",}
 
