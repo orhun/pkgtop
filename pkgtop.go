a1befb0883bf1fef60ba604608280cbb93be724d
KeyLo99 <keylo99official@gmail.com>
2019-07-31 03:15:24 +0300

    Add function comments to main function

--- pkgtop.go
+++ pkgtop.go
@@ -154,6 +154,9 @@ func execCmd(name string, arg ...string) string {
 	return string(out)
 }
 
+/*!
+ * Entry-point
+ */
 func main() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
