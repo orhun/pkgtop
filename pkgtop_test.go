4ba8ac7a441e66fc6ca5fadfb5bbb45f1c402bea
KeyLo99 <keylo99official@gmail.com>
2019-08-05 21:45:07 +0300

    Fix formatting of the test file

--- pkgtop_test.go
+++ pkgtop_test.go
@@ -1,7 +1,8 @@
 package main
+
 import (
-	"testing"
 	"strings"
+	"testing"
 )
 
 func TestGetDfEntries(t *testing.T) {
@@ -13,29 +14,29 @@ func TestGetDfEntries(t *testing.T) {
 	dfIndex := 0
 	dfCount := 3
 	gauges, dfEntries := getDfEntries(
-		dfOutput, 
-		dfIndex, 
+		dfOutput,
+		dfIndex,
 		dfCount)
-	if dfEntries == nil || len(dfEntries) != dfCount || 
+	if dfEntries == nil || len(dfEntries) != dfCount ||
 		len(gauges) != dfCount {
-		t.Errorf("Error occurred while parsing the 'df' values. " + 
-			"Expected length %d, got %d-%d", 
+		t.Errorf("Error occurred while parsing the 'df' values. "+
+			"Expected length %d, got %d-%d",
 			dfCount, len(gauges), len(dfEntries))
 	}
 }
 
 func TestGetPkgListEntries(t *testing.T) {
-	pkgs := []string {
+	pkgs := []string{
 		"val0~10~x~test1",
 		"val1~20~y~test2",
 		"[1]|[2]",
 	}
 	titles := strings.Split(pkgs[len(pkgs)-1], "|")
 	lists, entries := getPkgListEntries(pkgs)
-	if(len(lists) != len(titles) || 
-		len(entries) != len(titles)) {
-		t.Errorf("Error occurred while parsing the 'pkg' values. " + 
-			"Expected length %d, got %d-%d", 
+	if len(lists) != len(titles) ||
+		len(entries) != len(titles) {
+		t.Errorf("Error occurred while parsing the 'pkg' values. "+
+			"Expected length %d, got %d-%d",
 			len(titles), len(lists), len(entries))
 	}
 }
